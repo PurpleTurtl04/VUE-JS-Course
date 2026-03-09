5a6ad8661185c8a22b2e970542b661701c54b25d
Nick Edwards <nledwa01@gmail.com>
2026-03-09 15:25:54 -0500

    Completed videos 34-42. Completed Section 3

--- Section 3/comps/src/App.vue
+++ Section 3/comps/src/App.vue
@@ -2,40 +2,24 @@
     <div>
         <app-header />
         <div class="container">
-            <Cars />
-            <hr />
-            <p>Parent: {{ cars[0].brand }}-{{ cars[0].model }}</p>
-            <button @click="updateCar">Update from parent</button>
+            <Directives />
         </div>
     </div>
 </template>
 
 <script setup>
-import Cars from '@/components/Cars/index.vue';
-import { reactive, provide } from 'vue';
-
-const cars = reactive([
-    { model: 'F9', brand: 'Ferrari' },
-    { model: '911', brand: 'Porsche' },
-    { model: 'Tipo', brand: 'Fiat' },
-]);
-
-const updateCar = () => {
-    cars[0].brand = 'Ford';
-};
-
-provide('cars', { cars, updateCar });
+    import Directives from '@/components/Directives/index.vue';
 </script>
 
-<style>
-body {
-    padding: 0;
-    margin: 0;
-    font-family: 'Roboto', sans-serif;
-}
-.container {
-    min-height: 84vh;
-    box-sizing: border-box;
-    padding: 20px;
-}
+<style scoped>
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+    .container {
+        min-height: 84vh;
+        box-sizing: border-box;
+        padding: 20px;
+    }
 </style>
---
+++ Section 3/comps/src/components/Players/steve.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>Steve is on</h3>
+</template>
+
+<script setup>
+    import { onUnmounted, onActivated, onDeactivated } from 'vue';
+
+    onUnmounted(() => {
+        console.log('Steve unmounted');
+    });
+    onActivated(() => {
+        console.log('Steve onActivated');
+    });
+    onDeactivated(() => {
+        console.log('Steve onDeactivated');
+    });
+</script>
