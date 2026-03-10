6c2614ecdfcd9fb26f3ec819b12bb26933a8bf8d
Nick Edwards <nledwa01@gmail.com>
2026-03-10 16:17:59 -0500

    Added transitions solution files. Completed videos 48-52. Completed Section 4

---
+++ Section 4/transitions/src/components/transitions/one.vue
@@ -0,0 +1,117 @@
+<template>
+    <Transition>
+        <div class="p-3 mb-2 bg-success text-white" v-if="data.display">
+            Hello
+        </div>
+    </Transition>
+    <button class="btn btn-primary" @click="data.display = !data.display">
+        Toggle
+    </button>
+    <hr />
+    <Transition>
+        <div class="p-3 mb-2 bg-success text-white" v-if="data.display2">
+            Hello
+        </div>
+    </Transition>
+    <button class="btn btn-primary" @click="data.display2 = !data.display2">
+        Toggle Display 2
+    </button>
+    <hr />
+    <Transition name="slide">
+        <div class="p-3 mb-2 bg-success text-white" v-if="data.display3">
+            Hello
+        </div>
+    </Transition>
+    <button class="btn btn-primary" @click="data.display3 = !data.display3">
+        Toggle Display 3
+    </button>
+    <hr />
+    <Transition name="slideup">
+        <div class="p-3 mb-2 bg-success text-white" v-if="data.display4">
+            Hello
+        </div>
+    </Transition>
+    <button class="btn btn-primary" @click="data.display4 = !data.display4">
+        Toggle Display 4
+    </button>
+</template>
+
+<script setup>
+    import { reactive } from 'vue';
+
+    const data = reactive({
+        display: false,
+        display2: true,
+        display3: true,
+        display4: false,
+    });
+</script>
+
+<style scoped>
+    .v-enter-from,
+    .v-leave-to {
+        opacity: 0;
+    }
+    .v-enter-active,
+    .v-leave-active {
+        transition: 1s;
+    }
+    .v-enter-to,
+    .v-leave-from {
+        opacity: 1;
+    }
+
+    /* SLIDE */
+    .slide-enter-from {
+        transform: translateX(20px);
+        opacity: 0;
+    }
+    .slide-enter-active {
+        transition: all 1s ease;
+    }
+    .slide-enter-to {
+        opacity: 1;
+    }
+
+    .slide-leave-from {
+        opacity: 1;
+        background-color: red !important;
+    }
+
+    .slide-leave-active {
+        transition: all 0.3s ease;
+    }
+    .slide-leave-to {
+        background-color: black !important;
+        transform: translateX(100px);
+        opacity: 0;
+    }
+
+    .slideup-enter-active {
+        animation: slideUp 0.5s ease-in-out;
+    }
+    .slideup-leave-active {
+        animation: slideDown 0.5s ease-in-out;
+    }
+
+    @keyframes slideUp {
+        from {
+            opacity: 0;
+            transform: translateY(10px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+    @keyframes slideDown {
+        from {
+            opacity: 1;
+            transform: translateY(0);
+        }
+        to {
+            opacity: 0;
+            transform: translateY(10px);
+        }
+    }
+</style>
---
+++ Section 4/transitions/src/components/transitions/two.vue
@@ -0,0 +1,48 @@
+<template>
+    <button class="btn btn-primary" @click="status = !status">
+        Toggle status
+    </button>
+
+    <Transition
+        mode="out-in"
+        name="custom"
+        enter-active-class="dog"
+        leave-active-class="cat"
+    >
+        <div
+            class="p-3 mb-2 bg-danger text-white"
+            v-if="!status"
+            key="status_off"
+        >
+            OFF
+        </div>
+        <div
+            class="p-3 mb-2 bg-success text-white"
+            v-else="status"
+            key="status_on"
+        >
+            ON
+        </div>
+    </Transition>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const status = ref(false);
+</script>
+
+<style scoped>
+    .custom-enter-from,
+    .custom-leave-to {
+        opacity: 0;
+    }
+    .dog,
+    .cat {
+        transition: 1s;
+    }
+    .custom-enter-to,
+    .custom-leave-from {
+        opacity: 1;
+    }
+</style>
