1d14c4c470ef3eb02e3e88acae565e193c0b24b6
Nick Edwards <nledwa01@gmail.com>
2026-03-10 22:55:33 -0500

    Installed json server and set up db.json file. Completed videos 54-57.

--- Section 5/routes/src/components/header/index.vue
+++ Section 5/routes/src/components/header/index.vue
@@ -1,30 +1,38 @@
 <template>
     <div class="container">
-      <header
-        class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
-      >
-        <a
-          href="/"
-          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+        <header
+            class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"
         >
-          <span class="fs-4">Beardit.com</span>
-        </a>
-  
-        <ul class="nav nav-pills">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">Home</a>
-          </li>
-          <li class="nav-item"><a href="#" class="nav-link">Articles</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-        </ul>
-      </header>
+            <RouterLink
+                to="/"
+                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+            >
+                <span class="fs-4">Beardit.com</span>
+            </RouterLink>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <RouterLink to="/" class="nav-link">Home</RouterLink>
+                </li>
+                <li class="nav-item">
+                    <RouterLink to="/articles" class="nav-link"
+                        >Articles</RouterLink
+                    >
+                </li>
+                <li class="nav-item">
+                    <RouterLink to="/contact" class="nav-link"
+                        >Contact</RouterLink
+                    >
+                </li>
+            </ul>
+        </header>
     </div>
-  </template>
-  
-  <style>
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: "Roboto", sans-serif;
-  }
-  </style>
+</template>
+
+<style>
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
--- Section 5/routes/src/router/index.js
+++ Section 5/routes/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/home.vue';
 import Articles from '@/components/articles/index.vue';
+import Article from '@/components/articles/article.vue';
 import Contact from '@/components/contact/index.vue';
 
 const router = createRouter({
@@ -8,8 +9,10 @@ const router = createRouter({
     routes: [
         { path: '/', component: Home },
         { path: '/articles', component: Articles },
+        { path: '/articles/:articleID', component: Article },
         { path: '/contact', component: Contact },
     ],
+    linkActiveClass: 'active',
 });
 
 export default router;
