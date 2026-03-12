c30ea1d3ce57f7d2f2a80112c99f9c9c81523e5c
Nick Edwards <nledwa01@gmail.com>
2026-03-12 16:40:34 -0500

    Completed videos 64-66

--- Section 6/statem/src/components/admin/index.vue
+++ Section 6/statem/src/components/admin/index.vue
@@ -1,28 +1,68 @@
 <template>
     <div class="row">
-      <h1>Admin</h1>
-      <hr />
-      <div class="col-sm-5">
-        <p>
-          <strong> Change count:0 </strong>
-        </p>
-  
-        <div>
-          <label class="form-label">By how much</label>
-          <input type="number" class="form-control" v-model="data.amount" />
+        <h1>Admin</h1>
+        <hr />
+        <div class="col-sm-5">
+            <p>
+                <strong> Change count: {{ count }} </strong>
+            </p>
+
+            <!-- <div>
+                <label class="form-label">By how much</label>
+                <input
+                    type="number"
+                    class="form-control"
+                    v-model="data.amount"
+                />
+            </div>
+            <br /> -->
+
+            <button type="button" class="btn btn-primary me-3" @click="add">
+                +
+            </button>
+            <button
+                type="button"
+                class="btn btn-outline-secondary"
+                @click="subtract"
+            >
+                -
+            </button>
         </div>
-        <br />
-  
-        <button type="button" class="btn btn-primary me-3">+</button>
-        <button type="button" class="btn btn-outline-secondary">-</button>
-      </div>
+        <div>
+            <hr />
+            <p>{{ store.car.brand }}</p>
+            <p>{{ store.car.color }}</p>
+            <button
+                type="button"
+                class="btn btn-primary me-3"
+                @click="patchState"
+            >
+                Patch state
+            </button>
+        </div>
     </div>
-  </template>
-  
-  <script setup>
-    import { reactive } from 'vue'
-  
-    const data = reactive({
-      amount:1
-    })
-  </script>
+</template>
+
+<script setup>
+    import { computed } from 'vue';
+    import { useCounterStore } from '@/store/counter';
+
+    const store = useCounterStore();
+
+    const count = computed(() => store.getCount);
+    const add = () => {
+        store.counter++;
+    };
+    const subtract = () => {
+        store.counter--;
+    };
+
+    const patchState = () => {
+        store.$patch({
+            car: {
+                brand: 'Renault',
+                color: 'Blue',
+            },
+        });
+    };
+</script>
